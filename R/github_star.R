76d8a934f3a75771b872ad75c28389a807d01814
Steve Simpson <steventroysimpson@gmail.com>
2016-05-31 22:03:02 -0400

    fixed github_star

--- R/github_star.R
+++ R/github_star.R
@@ -15,7 +15,7 @@ github_star <- function(repo, acct="data-steve", size="mega",script=FALSE){
   star <- paste0('<a class="github-button" '
                  , 'href="https://github.com/'
                  , acct,"/",repo
-                 ,' data-icon="octicon-star"'
+                 ,'" data-icon="octicon-star"'
                  ,' data-style="', size
                  ,'">',repo,'</a>',
                  ifelse(script,'<script async defer src="https://buttons.github.io/buttons.js"></script>',"" ))
